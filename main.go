4987eb5057283dda089f7c1aabc66db318b4f808
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2021-03-05 20:48:43 +0900

    basic reader

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"http_saba/routes"
-
-	"github.com/gin-gonic/gin"
+	"compress/gzip"
+	"encoding/base64"
+	"io"
+	"os"
+	"strings"
 )
 
-func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("views/*.html")
-	router.Static("/assets", "./assets")
-
-	user := router.Group("/user")
-	{
-		user.POST("/signup", routes.UserSignUp)
-		user.POST("/login", routes.UserLogIn)
-	}
-
-	router.GET("/", routes.Home)
-	router.GET("/login", routes.LogIn)
-	router.GET("/signup", routes.SignUp)
-	router.NoRoute(routes.NoRoute)
-
-	router.Run(":8080")
+func main(){ 
+	var r io.Reader
+	r = strings.NewReader(data)
+	r = base64.NewDecoder(base64.StdEncoding, r)
+	r, _ = gzip.NewReader(r)
+	io.Copy(os.Stdout, r)
 }
+
+const data = H4sIAAAJbogA/1SOO5KDQAxE8zlFZ5tQXGCjjfYIjoURoPKgcY0E57f4VZlQXf2e+r8yOYbMZJhoZWRxz3wkCVjeReETS0VHz5fBCzpxxg/PbfrT/gacCjbjeiRNOChaVkA9RAdR8eVEw4vxa0Dcs3Fe2ZqowpeqG79L995l3VaMBUV/02OS+B6kMWikwG51c8n5GnEPr11F2/QJAAD//z9IppsHAQAA 
